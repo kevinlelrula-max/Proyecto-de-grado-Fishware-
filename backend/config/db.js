728091b00977e06d5462ec9df737f6b44a583767
kevinlelrula-max <kevinlelrula@gmail.com>
2026-08-01 18:43:35 -0500

    modificacion para soporte neon

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,7 @@ export const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
